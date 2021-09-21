c775ca68f0b4d3b03b76a4ab860aff1af1119fc9
cr1ogen <cr1ogen@protonmail.com>
2021-09-21 15:38:37 -0300

    Delete qtile directory

--- qtile/autostart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
-csd-xsettings &
-xscreensaver --no-splash &
-nitrogen --restore &
-/usr/bin/dunst &
-picom --experimental-backends &
-pasystray &
-qrazercfg-applet &
