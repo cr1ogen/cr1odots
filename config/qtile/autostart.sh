aefce06d89f08afd75e567e1901cee03cb934014
cr1ogen <cr1ogen@protonmail.com>
2021-12-19 14:00:15 -0300

    Add files via upload

--- config/qtile/autostart.sh
+++ config/qtile/autostart.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-#lxpolkit &
-/usr/lib/x86_64-linux-gnu/polkit-mate/polkit-mate-authentication-agent-1 &
-xrandr -s 3840x2160 --output DP-2 --scale 0.5x0.5 &
-picom --experimental-backends &
-nm-applet & 
-pasystray &
-dunst &
-wal -R &		
+wlr-randr --output DP-2 --scale 2 &
+#mpvpaper -f -o "loop" DP-2 /home/cr1ogen/Imágenes/Videowall/video.mp4 &
+#wal -R &
