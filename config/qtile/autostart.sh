c5f40a7003b5b59f4bec0f158658e65a914aaf1e
cr1ogen <cr1ogen@protonmail.com>
2022-04-04 02:25:42 -0300

    Add files via upload

---
+++ config/qtile/autostart.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+volumeicon &
+wlr-randr --output DP-2 --scale 2 &
+#feh --bg-scale ~/Imágenes/Wallpapers/city.jpeg &
+#mpvpaper -f -o "loop" DP-2 /home/cr1ogen/Imágenes/Videowall/video.mp4 &
+/usr/bin/emacs --daemon &
+dunst &
+conky-startup &
