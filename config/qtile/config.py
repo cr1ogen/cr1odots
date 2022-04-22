6fd8940138ff44e79cd9771dbd885b94e7157372
cr1ogen <cr1ogen@protonmail.com>
2022-04-22 15:33:40 -0300

    Add files via upload

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -11,6 +11,8 @@ import subprocess
 from libqtile import bar
 from libqtile.config import Screen
 from libqtile.lazy import lazy
+#from qtile_extras import widget as extrawidgets #Extras
+
 
 # Modules
 
@@ -22,7 +24,7 @@ from modules.layouts import layouts
 from modules.mouse import mouse
 from modules.widgets import primary_widgets
 
-colors = [["#282c34", "#282c34"],
+colors = [[],
           ["#1c1f24", "#1c1f24"],
           ["#dfdfdf", "#dfdfdf"],
           ["#ff6c6b", "#ff6c6b"],
@@ -33,16 +35,27 @@ colors = [["#282c34", "#282c34"],
           ["#46d9ff", "#46d9ff"],
           ["#a9a1e1", "#a9a1e1"]]
 
+## Colors from Pywal
+
+cache='/home/cr1ogen/.cache/wal/colors'
+def load_colors(cache):
+    with open(cache, 'r') as file:
+        for i in range(8):
+            colors.append(file.readline().strip())
+    colors.append('#ffffff')
+    lazy.reload()
+load_colors(cache)
+
 
 screens = [
     Screen(
-        wallpaper = '~/Imagenes/Wallpapers/Rain.jpg',
+        wallpaper = '~/Imagenes/Wallpapers/forest.jpg',
         wallpaper_mode = 'fill',
         top=bar.Bar(primary_widgets,
         size=26,
         opacity=0.90,
         background=colors[1],
-        border_width=[6, 6, 0, 6],  # Draw top and bottom borders
+        border_width=[6, 0, 0, 0],  # Draw top and bottom borders
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
     ),
