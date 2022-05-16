c220ff9bc7ebeee1ead1b28394598df54079d460
cr1ogen <cr1ogen@protonmail.com>
2022-05-15 21:30:55 -0300

    Add files via upload

--- config/qtile/modules/widgets.py
+++ config/qtile/modules/widgets.py
@@ -5,7 +5,7 @@ from libqtile import widget
 from qtile_extras import widget as extrawidgets #Extras
 
 widget_defaults = dict(
-    font="JetBrainsMonoExtraBold",
+    font="JetBrainsMono Nerd Font",
     fontsize=12,
     padding=3,
 )
@@ -13,30 +13,34 @@ extension_defaults = widget_defaults.copy()
 
 primary_widgets = [
                 widget.GroupBox(
-                fontsize=26,
+                fontsize=28,
+                padding=7,
                 highlight_method='text',
                 ),
                 widget.Prompt(
                 ),
                 widget.TaskList(
+                    font="JetBrainsMono Nerd Font",
                     highlight_method='text',
                     max_title_width=200,
                 ),
                 widget.Clock(
                     format="%b  %d %H:%M %p",
-                    font='JetBrains bold',
+                    font='JetBrainsMono Nerd Font Bold',
+                    fontsize=14,
                 ),
                 widget.Spacer(
                 ),
                 extrawidgets.StatusNotifier(
+                icon_size=22,
                 ),
                 extrawidgets.ALSAWidget(
                     mode='icon',
-                    theme_path='/home/cr1ogen/.local/share/icons/McMuse-orange-dark',
+                    theme_path='/home/cr1ogen/.local/share/icons/BeautyLine',
                     mouse_callbacks={'Button3': lambda: qtile.cmd_spawn("pavucontrol")},
                 ),
                  widget.CurrentLayoutIcon(
-                    scale=0.5,
+                    scale=0.7,
                     custom_icon_paths=["/home/cr1ogen/.config/qtile/layout-icons/gruvbox-neutral_orange"],
                 ),
 
