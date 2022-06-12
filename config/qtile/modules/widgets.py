c477bddd34cfcfc9399844e8ca3229dc28f10ace
cr1ogen <cr1ogen@protonmail.com>
2022-06-12 20:16:55 -0300

    Add files via upload

--- config/qtile/modules/widgets.py
+++ config/qtile/modules/widgets.py
@@ -13,31 +13,41 @@ extension_defaults = widget_defaults.copy()
 
 primary_widgets = [
                 widget.GroupBox(
-                fontsize=28,
-                padding=7,
-                highlight_method='text',
+                    font="JetBrainsMono Nerd Font",
+                    fontsize=18,
+                    padding=7,
+                    highlight_method='text',
                 ),
                 widget.Prompt(
                 ),
                 widget.TaskList(
                     font="JetBrainsMono Nerd Font",
+                    fontsize=14,
                     highlight_method='text',
                     max_title_width=200,
                 ),
                 widget.Clock(
-                    format="%b  %d %H:%M %p",
+                    format=' %d %b, %H:%M %p',
                     font='JetBrainsMono Nerd Font Bold',
                     fontsize=14,
                 ),
                 widget.Spacer(
                 ),
                 extrawidgets.StatusNotifier(
-                icon_size=22,
+                    icon_size=22,
+                ),
+                widget.TextBox(
+                    text='',
+                    fontsize=24,
+                ),
+                widget.KeyboardLayout(
+                configured_keyboards=['us','es'],
                 ),
                 extrawidgets.ALSAWidget(
                     mode='icon',
                     theme_path='/home/cr1ogen/.local/share/icons/BeautyLine',
                     mouse_callbacks={'Button3': lambda: qtile.cmd_spawn("pavucontrol")},
+                    margin=13,
                 ),
                  widget.CurrentLayoutIcon(
                     scale=0.7,
