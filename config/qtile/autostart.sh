092f245fd0f8e79b4587954f80f2841e5d8754d6
cr1ogen <cr1ogen@protonmail.com>
2023-01-17 21:30:16 -0300

    Add files via upload

--- config/qtile/autostart.sh
+++ config/qtile/autostart.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-volumeicon &
-wlr-randr --output DP-2 --scale 2 &
+#volumeicon &
+
+#scale in X11
+xrandr --output DisplayPort-2 --scale 0.60 &
+
+#scale in wayland
+wlr-randr --output DP-3 --scale 1.75 &
+
+exec /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+
 #feh --bg-scale ~/Imágenes/Wallpapers/city.jpeg &
 #mpvpaper -f -o "loop" DP-2 /home/cr1ogen/Imágenes/Videowall/video.mp4 &
-/usr/bin/emacs --daemon &
+#/usr/bin/emacs --daemon &
 dunst &
-conky-startup &
+#conky-startup &
