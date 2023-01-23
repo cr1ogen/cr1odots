63c61bf47f521ecf5038ac75243d9d046c1ce7a6
cr1ogen <cr1ogen@protonmail.com>
2023-01-23 13:50:25 -0300

    Update screens.py

--- config/qtile/modules/screens.py
+++ config/qtile/modules/screens.py
@@ -5,13 +5,13 @@ from modules.colors import colors
 
 screens = [
     Screen(
-        wallpaper = '~/.local/share/backgrounds/Red.jpg',
+        wallpaper = '~/.local/share/backgrounds/resto.png',
         wallpaper_mode = 'fill',
         top=bar.Bar(primary_widgets,
         size=26,
         opacity=1,
         background=colors['dark'],
-        margin=[8, 14, 0, 14], 
+        margin=[8, 13, 0, 13], 
         ),
     ),
 ]
