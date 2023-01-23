617b16ef27e4568232b50e118395fb3efa241adb
cr1ogen <cr1ogen@protonmail.com>
2023-01-23 13:48:31 -0300

    Add files via upload

--- config/qtile/modules/layouts.py
+++ config/qtile/modules/layouts.py
@@ -1,12 +1,24 @@
 from libqtile import layout
 from libqtile.config import Match
+from modules.colors import colors
 
 layouts = [
-    layout.MonadTall(border_focus_stack=["#d75f5f", "#8f3d3d"],
-                   border_width=1,
-                   margin=6,
+    layout.MonadTall(
+        border_focus=colors['orange'],
+        border_width=1,
+        margin=14,
+                   ),
+    layout.Floating(
+        border_focus=colors['orange'],
+        border_normal=colors['orange'],
                    ),
     layout.Max(),
+    layout.Spiral(
+        border_focus=colors['orange'],
+        main_pane='bottom',
+        border_width=1,
+        margin=14,
+                   ),
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
     # layout.Bsp(),
@@ -27,8 +39,8 @@ floating_layout = layout.Floating(
         Match(wm_class="confirmreset"),  # gitk
         Match(wm_class="makebranch"),  # gitk
         Match(wm_class="maketag"),  # gitk
-        Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(wm_class="Kitty"),  # ssh-askpass
         Match(title="branchdialog"),  # gitk
         Match(title="pinentry"),  # GPG key password entry
-    ]
+        ],
 )
