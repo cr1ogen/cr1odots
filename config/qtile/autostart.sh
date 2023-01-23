724536c32d594abc0a7d05dd5a1aa35cc3cb59be
cr1ogen <cr1ogen@protonmail.com>
2023-01-23 13:44:38 -0300

    Update autostart.sh

--- config/qtile/autostart.sh
+++ config/qtile/autostart.sh
@@ -8,6 +8,10 @@ xrandr --output DisplayPort-2 --scale 0.60 &
 #scale in wayland
 wlr-randr --output DP-3 --scale 1.75 &
 
+# Share screen with pipewire hack
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots &
+systemctl --user stop  xdg-desktop-portal xdg-desktop-portal-wlr &
+
 exec /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 
 #feh --bg-scale ~/Imágenes/Wallpapers/city.jpeg &
