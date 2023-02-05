57b87f02b35dc3f9bf27e36c0365680b303649a3
cr1ogen <cr1ogen@protonmail.com>
2023-02-05 01:33:35 -0300

    Update groups.py

--- config/qtile/modules/groups.py
+++ config/qtile/modules/groups.py
@@ -3,17 +3,16 @@ from libqtile.command import lazy
 from .keys import mod, keys
 
 groups = [
-    Group(name='1', label="", layout="monadtall"),
-    Group(name='2', label="", matches=[
-          Match(wm_class='qutebrowser')], layout="monadtall"),
-    Group(name='3', label="", matches=[
-          Match(wm_class='Alacritty'), Match(wm_class='code-git')], layout="monadtall"),
+    Group(name='1', label="", matches=[
+          Match(wm_class='firefox')], layout="monadtall"),
+    Group(name='2', label="", layout="monadtall"),
+    Group(name='3', label="", layout="monadtall"),
     Group(name='4', label="", matches=[
-          Match(wm_class='discord')], layout='max'),
+          Match(wm_class='discord')], layout='monadtall'),
     Group(name='5', label="ﰝ", matches=[
-        Match(wm_class='vlc')], layout='max'),
+        Match(wm_class='pw-jack ardour')], layout='max'),
     Group(name='6', label="", matches=[
-        Match(wm_class='Steam')], layout='monadtall'),
+        Match(wm_class='Steam'), Match(wm_class='r2modman'), Match(wm_class='heroic')], layout='monadtall'),
 
 ]
 
