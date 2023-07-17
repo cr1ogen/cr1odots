a6258fb0a4b0b4df6d5c26af641c6bf950a56c08
cr1ogen <cr1ogen@protonmail.com>
2023-07-16 22:37:14 -0300

    Add files via upload

--- config/qtile/modules/groups.py
+++ config/qtile/modules/groups.py
@@ -4,15 +4,15 @@ from .keys import mod, keys
 
 groups = [
     Group(name='1', label="", matches=[
-          Match(wm_class='firefox')], layout="monadtall"),
+        Match(wm_class='firefox')], layout="monadtall"),
     Group(name='2', label="", layout="monadtall"),
     Group(name='3', label="", layout="monadtall"),
     Group(name='4', label="", matches=[
-          Match(wm_class='discord')], layout='monadtall'),
+          Match(wm_class='discord')], layout='max'),
     Group(name='5', label="ﰝ", matches=[
         Match(wm_class='pw-jack ardour')], layout='max'),
     Group(name='6', label="", matches=[
-        Match(wm_class='Steam'), Match(wm_class='r2modman'), Match(wm_class='heroic')], layout='monadtall'),
+        Match(wm_class='iniciar sesión en steam'), Match(wm_class='steam'), Match(wm_class='r2modman'), Match(wm_class='heroic')], layout='monadtall'),
 
 ]
 
