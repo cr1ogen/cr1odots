0ffcea16f523c0a857c1b59f5218e555a4c2461e
cr1ogen <cr1ogen@protonmail.com>
2024-05-27 12:27:38 -0300

    Update autostart.sh

--- config/qtile/autostart.sh
+++ config/qtile/autostart.sh
@@ -4,7 +4,7 @@
 xrandr --output DisplayPort-2 --scale 0.60 &
 
 #scale in wayland
-wlr-randr --output DP-3 --scale 1.75 &
+wlr-randr --output DP-4 --scale 1.75 
 
 # Share screen with pipewire hack
 dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots &
@@ -12,5 +12,5 @@ systemctl --user stop  xdg-desktop-portal xdg-desktop-portal-wlr &
 
 exec /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 
-mpvpaper -vfs -o "loop" DP-3 /home/cr1ogen/.local/share/backgrounds/live/Ninja.mp4 &
+mpvpaper -vfs -o "loop" DP-4 /home/cr1ogen/.local/share/backgrounds/live/Ninja.mp4 &
 dunst &
