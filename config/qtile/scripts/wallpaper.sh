465cfb00f0f9274064ca78252339cefc8c8a3666
cr1ogen <cr1ogen@protonmail.com>
2024-10-06 21:52:32 -0300

    Add files via upload

--- config/qtile/scripts/wallpaper.sh
+++ config/qtile/scripts/wallpaper.sh
@@ -30,9 +30,14 @@ generated_versions="$HOME/.cache/wallpaper-generated"
 waypaper_running=$HOME/.cache/waypaper-running
 cache_file="$HOME/.cache/current_wallpaper"
 default_wallpaper="$HOME/.local/share/backgrounds/default.jpg"
-#wallpaper_effect="$HOME/.config/ml4w/settings/wallpaper-effect.sh"
-#blur="50x30"
-#blur=$(cat $blur_file)
+wallpaper_effect="$HOME/.local/bin/wallpaper-effect.sh"
+rasi_file="$HOME/.cache/current_wallpaper.rasi"
+blur_file="$HOME/.local/bin/blur.sh"
+blurred_wallpaper="$HOME/.cache/blurred_wallpaper.png"
+square_wallpaper="$HOME/.cache/square_wallpaper.png"
+
+blur="50x30"
+blur=$(cat $blur_file)
 
 if [ -f $waypaper_running ] ;then 
     rm $waypaper_running
@@ -79,28 +84,7 @@ echo ":: Path of current wallpaper copied to $cache_file"
 wallpaper_filename=$(basename $wallpaper)
 echo ":: Wallpaper Filename: $wallpaper_filename"
 
-# ----------------------------------------------------- 
-# Wallpaper Effects
-# -----------------------------------------------------
 
-#if [ -f $wallpaper_effect ] ;then
-#    effect=$(cat $wallpaper_effect)
-#    if [ ! "$effect" == "off" ] ;then
-#        used_wallpaper=$generated_versions/$effect-$wallpaper_filename
-#        if [ -f $generated_versions/$effect-$wallpaper_filename ] && [ "$force_generate" == "0" #] && [ "$use_cache" == "1" ] ;then
-#            echo ":: Use cached wallpaper $effect-$wallpaper_filename"
-#        else
-#            echo ":: Generate new cached wallpaper $effect-$wallpaper_filename with effect $effe#ct"
-#            dunstify "Using wallpaper effect $effect..." "with image $wallpaper_filename" -h int#:value:10 -h string:x-dunst-stack-tag:wallpaper
-#            source $HOME/.config/hypr/effects/wallpaper/$effect
-#        fi
-#        echo ":: Loading wallpaper $generated_versions/$effect-$wallpaper_filename with effect $#effect"
-#        echo ":: Setting wallpaper with $used_wallpaper"
-#waypaper --wallpaper $used_wallpaper
-#    else
-#        echo ":: Wallpaper effect is set to off"
-#    fi
-#fi
 
 # ----------------------------------------------------- 
 # Execute pywal
@@ -119,29 +103,29 @@ qtile cmd-obj -o cmd -f reload_config
 # Created blurred wallpaper
 # -----------------------------------------------------
 
-#echo ":: Generate new cached wallpaper blur-$blur-$wallpaper_filename with blur $blur"
-#magick $used_wallpaper -resize 75% $blurred_wallpaper
-#echo ":: Resized to 75%"
-#if [ ! "$blur" == "0x0" ] ;then
-#    magick $blurred_wallpaper -blur $blur $blurred_wallpaper
-#    cp $blurred_wallpaper $generated_versions/blur-$blur-$wallpaper_filename.png
-#    echo ":: Blurred"
-#fi
-#cp $generated_versions/blur-$blur-$wallpaper_filename.png $blurred_wallpaper
+echo ":: Generate new cached wallpaper blur-$blur-$wallpaper_filename with blur $blur"
+convert $used_wallpaper -resize 75% $blurred_wallpaper
+echo ":: Resized to 75%"
+if [ ! "$blur" == "0x0" ] ;then
+    convert $blurred_wallpaper -blur $blur $blurred_wallpaper
+    cp $blurred_wallpaper $generated_versions/blur-$blur-$wallpaper_filename.png
+    echo ":: Blurred"
+fi
+cp $generated_versions/blur-$blur-$wallpaper_filename.png $blurred_wallpaper
 
 # ----------------------------------------------------- 
 # Create rasi file
 # ----------------------------------------------------- 
 
-#if [ ! -f $rasi_file ] ;then
-#    touch $rasi_file
-#fi
-#echo "* { current-image: url(\"$blurred_wallpaper\", height); }" > "$rasi_file"
+if [ ! -f $rasi_file ] ;then
+    touch $rasi_file
+fi
+echo "* { current-image: url(\"$blurred_wallpaper\", height); }" > "$rasi_file"
 
 # ----------------------------------------------------- 
 # Created square wallpaper
 # -----------------------------------------------------
 
-#echo ":: Generate new cached wallpaper square-$wallpaper_filename"
-#convert $tmp_wallpaper -gravity Center -extent 1:1 $square_wallpaper
-#cp $square_wallpaper $generated_versions/square-$wallpaper_filename.png
+echo ":: Generate new cached wallpaper square-$wallpaper_filename"
+convert $tmp_wallpaper -gravity Center -extent 1:1 $square_wallpaper
+cp $square_wallpaper $generated_versions/square-$wallpaper_filename.png
