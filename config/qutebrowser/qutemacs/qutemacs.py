deef294f1126a0bff93f63f1493af0a95afce701
cr1ogen <cr1ogen@protonmail.com>
2025-08-24 19:30:17 -0300

    Add files via upload

--- config/qutebrowser/qutemacs/qutemacs.py
+++ config/qutebrowser/qutemacs/qutemacs.py
@@ -1,175 +1,132 @@
-# qutemacs - un conjunto de enlaces Emacs simples y preconfigurados para qutebrowser
+# qutemacs - a simple, preconfigured Emacs binding set for qutebrowser
 #
-# El objetivo de este conjunto de enlaces no es proporcionar enlaces para absolutamente
-# todo, sino proporcionar un punto de partida estable para que la gente haga sus
-# propios enlaces.
+# The aim of this binding set is not to provide bindings for absolutely
+# everything, but to provide a stable launching point for people to make their
+# own bindings.
 #
-# Instalación:
+# Installation:
 #
-# 1. Copia este archivo o agrega este repositorio como un submódulo a tus dotfiles.
-# 2. Agrega esta línea a tu config.py y apunta la ruta a este archivo:
+# 1. Copy this file or add this repo as a submodule to your dotfiles.
+# 2. Add this line to your config.py, and point the path to this file:
 # config.source('qutemacs/qutemacs.py')
 
 
 config = config  # type: ConfigAPI # noqa: F821 pylint: disable=E0602,C0103
 c = c  # type: ConfigContainer # noqa: F821 pylint: disable=E0602,C0103
 
-c.tabs.background = True
-# desactiva el modo insertar por completo
+# disable insert mode completely
 c.input.insert_mode.auto_enter = False
 c.input.insert_mode.auto_leave = False
 c.input.insert_mode.plugins = False
 
-# Reenvía las teclas sin enlazar
+# Forward unbound keys
 c.input.forward_unbound_keys = "all"
-
-
 
 ESC_BIND = 'clear-keychain ;; search ;; fullscreen --leave'
 
-
-import string
-
 c.bindings.default['normal'] = {}
-c.bindings.default['insert'] = {}
-
-c.bindings.commands['insert'] = {
-    '<ctrl-space>': 'leave-mode',
-    '<ctrl-g>': 'leave-mode;;fake-key <Left>;;fake-key <Right>',
-    '<ctrl-f>': 'fake-key <Shift-Right>', 
-    '<ctrl-b>': 'fake-key <Shift-Left>',
-    '<ctrl-e>': 'fake-key <Shift-End>',
-    '<ctrl-a>': 'fake-key <Shift-Home>',
-    '<ctrl-p>': 'fake-key <Shift-Up>',
-    '<ctrl-n>': 'fake-key <Shift-Down>',
-    '<Return>': 'leave-mode',
-    '<ctrl-w>': 'fake-key <Ctrl-x>;;message-info "cut to clipboard";;leave-mode',
-    '<alt-w>': 'fake-key <Ctrl-c>;;message-info "copy to clipboard";;leave-mode',
-    '<backspace>': 'fake-key <backspace>;;leave-mode',
-    '<alt-x>': 'leave-mode;;set-cmd-text :',
-    '<alt-o>': 'leave-mode;;tab-focus last',
-    '<Tab>': 'fake-key <f1>'
-    }
-
-
-
-
-for char in list(string.ascii_lowercase):
-    c.bindings.commands['insert'].update({char: 'fake-key ' + char + ';;leave-mode'})
-
-for CHAR in list(string.ascii_uppercase):
-    c.bindings.commands['insert'].update({CHAR: 'fake-key ' + char + ';;leave-mode'})
-
-for num in list(map(lambda x : str(x), range(0, 10))):
-    c.bindings.commands['insert'].update({num: 'fake-key ' + num + ';;leave-mode'})
-        
-for symb in [',', '.', '/', '\'', ';', '[', ']', '\\',
-             '!', '@','#','$','%','^','&','*','(',')','-','_', '=', '+', '`', '~',
-             ':', '\"', '<', '>', '?','{', '}', '|']:
-    c.bindings.commands['insert'].update({symb: 'insert-text ' + symb + ' ;;leave-mode'})
-
-
-# Enlaces
+# Bindings
 c.bindings.commands['normal'] = {
-    # Navegación
-    '<ctrl-space>': 'enter-mode insert',
-    '<ctrl-]>': 'fake-key <Ctrl-Shift-Right>',
-    '<ctrl-[>': 'fake-key <Ctrl-Shift-Left>',
-    '<ctrl-v>': 'scroll-page 0 0.5',
-    '<alt-v>': 'scroll-page 0 -0.5',
-    '<ctrl-shift-v>': 'scroll-page 0 1',
-    '<alt-shift-v>': 'scroll-page 0 -1',
 
-    '<alt-x>': 'set-cmd-text :',
-    '<ctrl-x>b': 'set-cmd-text -s :buffer;;fake-key <Down><Down><Down>',
-    '<ctrl-x>k': 'tab-close',
-    '<ctrl-x>r': 'config-cycle statusbar.hide',
-    '<ctrl-x>1': 'tab-only;;message-info "cleared all other tabs"',
-    '<ctrl-x><ctrl-c>': 'quit',
+	# Navigation
+	'<ctrl-v>': 'scroll-page 0 0.5',
+	'<alt-v>': 'scroll-page 0 -0.5',
+	'<ctrl-shift-v>': 'scroll-page 0 1',
+	'<alt-shift-v>': 'scroll-page 0 -1',
+	# FIXME come up with logical bindings for scrolling left/right
+        
+	# Commands
+        '<shift-:>':'set-cmd-text -s :',
+	'<ctrl-x>b': 'set-cmd-text -s :buffer',
+	'<ctrl-x>k': 'tab-close',
+        '<F5>': 'reload',
+        # Quit Qutebrowser
+	'<ctrl-x><ctrl-c>': 'quit',
+        '<ctrl-x><ctrl-s>': 'quit --save',
+        
 
-	# buscando
-    '<ctrl-s>': 'set-cmd-text /',
-    '<ctrl-r>': 'set-cmd-text ?',
+	# searching
+	'<ctrl-s>': 'cmd-set-text /',
+	'<ctrl-r>': 'cmd-set-text ?',
 
-	# dando pistas
-    '<alt-s>': 'hint all',
+	# hinting
+	'<alt-s>': 'hint all',
 
-    # pestañas
-    '<ctrl-tab>': 'tab-next',
-    '<ctrl-shift-tab>': 'tab-prev',
+	# history
+	# FIXME maybe this should be <C-b> <C-n>? Or would that be too confusing?
+	'<ctrl-]>': 'forward',
+	'<ctrl-[>': 'back',
 
-    # abrir enlaces
-    '<ctrl-l>': 'set-cmd-text -s :open',
-    '<alt-l>': 'set-cmd-text -s :open -t',
+	# tabs
+	'<ctrl-tab>': 'tab-next',
+	'<ctrl-shift-tab>': 'tab-prev',
+        '<ctrl-t>':'open -t', 
 
-    # editando
-    '<alt-[>': 'back',
-    '<alt-]>': 'forward',
-    '<ctrl-/>': 'fake-key <Ctrl-z>',
-    '<ctrl-shift-?>': 'fake-key <Ctrl-Shift-z>',
-    '<ctrl-k>': 'fake-key <Shift-End>;;fake-key <Backspace>',
-    '<ctrl-f>': 'fake-key <Right>',
-    '<ctrl-b>': 'fake-key <Left>',
-    '<alt-o>': 'tab-focus last',
-    '<ctrl-a>': 'fake-key <Home>',
-    '<ctrl-x>h': 'fake-key <Ctrl-a>',
-    '<ctrl-e>': 'fake-key <End>',
-    '<ctrl-n>': 'fake-key <Down>',
-    '<ctrl-p>': 'fake-key <Up>',
-    '<alt-f>': 'fake-key <Ctrl-Right>',
-    '<alt-b>': 'fake-key <Ctrl-Left>',
-    '<ctrl-d>': 'fake-key <Delete>',
-    '<alt-d>': 'fake-key <Ctrl-Delete>',
-    '<alt-backspace>': 'fake-key <Ctrl-Backspace>',
-    '<ctrl-w>': 'fake-key <Ctrl-x>;;message-info "cut to clipboard"',
-    '<alt-w>': 'fake-key <Ctrl-c>;;message-info "copy to clipboard"',
-    '<ctrl-y>': 'insert-text {primary}',
+	# open links
+	'<ctrl-o>': 'set-cmd-text -s :open',
+	'<ctrl-shift-o>': 'set-cmd-text -s :open -t',
+        '<ctrl-shift-p>': 'set-cmd-text -s :open --private',
 
-    '1': 'fake-key 1',
-    '2': 'fake-key 2',
-    '3': 'fake-key 3',
-    '4': 'fake-key 4',
-    '5': 'fake-key 5',
-    '6': 'fake-key 6',
-    '7': 'fake-key 7',
-    '8': 'fake-key 8',
-    '9': 'fake-key 9',
-    '0': 'fake-key 0',
+	# editing
+	'<ctrl-f>': 'fake-key <Right>',
+	'<ctrl-b>': 'fake-key <Left>',
+	'<ctrl-a>': 'fake-key <Home>',
+	'<ctrl-e>': 'fake-key <End>',
+	'<ctrl-n>': 'fake-key <Down>',
+	'<ctrl-p>': 'fake-key <Up>',
+	'<alt-f>': 'fake-key <Ctrl-Right>',
+	'<alt-b>': 'fake-key <Ctrl-Left>',
+	'<ctrl-d>': 'fake-key <Delete>',
+	'<alt-d>': 'fake-key <Ctrl-Delete>',
+	'<alt-backspace>': 'fake-key <Ctrl-Backspace>',
+	'<ctrl-w>': 'fake-key <Ctrl-backspace>',
+	'<ctrl-y>': 'insert-text {primary}',
+
+	# Numbers
+	# https://github.com/qutebrowser/qutebrowser/issues/4213
+	'1': 'fake-key 1',
+	'2': 'fake-key 2',
+	'3': 'fake-key 3',
+	'4': 'fake-key 4',
+	'5': 'fake-key 5',
+	'6': 'fake-key 6',
+	'7': 'fake-key 7',
+	'8': 'fake-key 8',
+	'9': 'fake-key 9',
+	'0': 'fake-key 0',
 
 	# escape hatch
-    '<ctrl-h>': 'set-cmd-text -s :help',
-    '<ctrl-g>': ESC_BIND,
+	'<ctrl-h>': 'set-cmd-text -s :help',
+	'<ctrl-g>': ESC_BIND,
 }
 
 c.bindings.commands['command'] = {
-    '<ctrl-s>': 'search-next',
-    '<ctrl-r>': 'search-prev',
+	'<ctrl-s>': 'search-next',
+	'<ctrl-r>': 'search-prev',
 
-    '<ctrl-p>': 'completion-item-focus prev',
-    '<ctrl-n>': 'completion-item-focus next',
+	'<ctrl-p>': 'completion-item-focus prev',
+	'<ctrl-n>': 'completion-item-focus next',
 
-    '<alt-p>': 'command-history-prev',
-    '<alt-n>': 'command-history-next',
-
+	'<alt-p>': 'command-history-prev',
+	'<alt-n>': 'command-history-next',
+        
 	# escape hatch
-    '<ctrl-g>': 'leave-mode',
+	'<ctrl-g>': 'leave-mode',
 }
 
 c.bindings.commands['hint'] = {
-    # escape hatch
-    '<ctrl-g>': 'leave-mode',
+	# escape hatch
+	'<ctrl-g>': 'leave-mode',
 }
 
 
 c.bindings.commands['caret'] = {
-    # escape hatch
-    '<ctrl-g>': 'leave-mode',
-    '<ctrl-space>': 'toggle-selection'
+	# escape hatch
+	'<ctrl-g>': 'leave-mode',
 }
 
-config.bind('<Tab>', 'fake-key <f1>')
-config.bind('<Ctrl-x><Ctrl-l>', 'config-source')
-c.tabs.show = 'always'
-#c.statusbar.hide = False
-c.url.searchengines["g"] = "https://www.google.com.ar/search?q={}"
-c.url.searchengines["DEFAULT"] = "https://www.google.com.ar/search?q={}"
+# Search engins
+c.url.searchengines = {
+    "DEFAULT": "https://duckduckgo.com/?q={}",
+    "g": "https://google.com/search?q={}",
+} 
