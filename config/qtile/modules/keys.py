ca9f4b3d7aae0410091561d74b649386385223a1
cr1ogen <cr1ogen@protonmail.com>
2026-02-11 01:35:25 -0300

    Change file manager shortcut from Thunar to Nemo

--- config/qtile/modules/keys.py
+++ config/qtile/modules/keys.py
@@ -57,7 +57,7 @@ keys = [
     Key([mod], "b", lazy.spawn("rofi -show drun"), desc="Launch apps"),
     Key([mod, "shift"], "b", lazy.spawn("rofi -show run"), desc="Laucnh commands"),
     Key([mod], "backslash", lazy.spawn("firefox")),
-    Key([mod, "shift"], "backslash", lazy.spawn("thunar")),
+    Key([mod, "shift"], "backslash", lazy.spawn("nemo")),
 
     #Captura de  Pantalla
     Key([], "Print", lazy.spawn("/home/cr1ogen/.local/bin/screenshot")),
