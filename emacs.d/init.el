481b90a3521cf41650530b4712121658c1a7513e
cr1ogen <cr1ogen@protonmail.com>
2026-06-22 10:51:08 -0300

    Add files via upload

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -73,7 +73,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(ewal-doom-themes ewal-evil-cursors ewal-spacemacs-themes treemacs)))
+   '(ewal ewal-doom-themes ewal-evil-cursors ewal-spacemacs-themes
+	  treemacs)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
