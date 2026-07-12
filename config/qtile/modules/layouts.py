86301f8a8912d282d60a44a53a52f435961c4eed
cr1ogen <cr1ogen@protonmail.com>
2026-07-12 01:22:51 -0300

    Add Nautilus to floating window rules

--- config/qtile/modules/layouts.py
+++ config/qtile/modules/layouts.py
@@ -29,12 +29,13 @@ floating_layout = layout.Floating(
     float_rules = [
         # Run the utility of `xprop` to see the wm class and name of an X client.
         *layout.Floating.default_float_rules,
-        Match(wm_class="kitty"),# gitk
+        Match(wm_class="kitty"),
+        Match(wm_class="org.gnome.Nautilus"),
         Match(wm_class="com.saivert.pwvucontrol"),
         Match(wm_class="confirmreset"),  # gitk
         Match(wm_class="makebranch"),  # gitk
         Match(wm_class="maketag"),  # gitk
-        Match(wm_class="waypaper"),  # ssh-askpass
+        Match(wm_class="waypaper"),  
         Match(title="branchdialog"),  # gitk
         Match(title="pinentry"),  # GPG key password entry
         ],
