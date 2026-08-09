7da22410ca608b5795e7a81e18ee3d73ffc9c693
cr1ogen <cr1ogen@protonmail.com>
2026-08-09 00:48:58 -0300

    Add files via upload

--- config/qtile/modules/layouts.py
+++ config/qtile/modules/layouts.py
@@ -35,7 +35,8 @@ floating_layout = layout.Floating(
         Match(wm_class="confirmreset"),  # gitk
         Match(wm_class="makebranch"),  # gitk
         Match(wm_class="maketag"),  # gitk
-        Match(wm_class="waypaper"),  
+        Match(wm_class="waypaper"),
+        Match(wm_class="xfce-polkit"),
         Match(title="branchdialog"),  # gitk
         Match(title="pinentry"),  # GPG key password entry
         ],
