b07b2924c58ef162ac9d157e57db1c54b8a06ca0
cr1ogen <cr1ogen@protonmail.com>
2026-08-29 14:41:52 -0300

    Add files via upload

---
+++ config/fabric/launch.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# 1. Matar SOLO las aplicaciones de Python de Fabric para no suicidarse
+# Usamos -o para asegurarnos de no cerrar este script de Bash actual
+pkill -o -f "python3.*fabric"
+sleep 0.5
+
+# 2. Definir la ruta base de tus widgets
+FABRIC_DIR="$HOME/.config/fabric"
+
+# 3. Encender tus widgets en paralelo (con el '&' al final)
+python3 "$FABRIC_DIR/Notificaciones/config.py" &
+
+# --- Tus futuros widgets ---
+# python3 "$FABRIC_DIR/barra_estado/config.py" &
+
+echo "¡Todos los widgets de Fabric han sido inicializados con éxito!"
