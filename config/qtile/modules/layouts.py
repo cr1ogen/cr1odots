2d08201992e354d51a72dac414616e292ebf3ff0
cr1ogen <cr1ogen@protonmail.com>
2026-09-07 00:52:10 -0300

    Implement window decorations with RoundedCorners
    Added RoundedCorners decoration for focused and inactive windows.

--- config/qtile/modules/layouts.py
+++ config/qtile/modules/layouts.py
@@ -1,14 +1,31 @@
-from libqtile import layout  
+from libqtile import layout
+from qtile_extras.layout.decorations import RoundedCorners
 from libqtile.config import Match
 from modules.colors import colors
 
 # Setup layout Theme
 
+# 1. Decoración para la ventana enfocada
+borde_enfocado = RoundedCorners(
+    #corner_radius=180,
+    #border_width=6,
+    colour=colors["primary"]
+)
+
+# 2. Decoración para las ventanas inactivas
+borde_normal = RoundedCorners(
+    #corner_radius=180,
+    #border_width=6,
+    colour=colors["surface_highest"] # Mismo radio y grosor, cambia el color
+)
+
 layout_theme = {
      "border_width":6,
      "margin":16,
-     "border_focus":colors["primary"], #['orange'],
-     "border_normal":colors["surface_highest"], #['dark'],
+     #"border_focus":colors["primary"], #['orange'],
+     "border_focus": borde_enfocado,
+     "border_normal": borde_normal,  
+     #"border_normal":colors["surface_highest"], #['dark'],
      "single_border_width": 8,
  }
 
