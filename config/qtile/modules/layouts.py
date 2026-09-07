12b6243ba665e2577b19526983fa177682c88dde
cr1ogen <cr1ogen@protonmail.com>
2026-09-07 12:43:51 -0300

    Clean up layout theme border properties
    Removed commented-out border properties for focused and normal windows.

--- config/qtile/modules/layouts.py
+++ config/qtile/modules/layouts.py
@@ -7,25 +7,19 @@ from modules.colors import colors
 
 # 1. Decoración para la ventana enfocada
 borde_enfocado = RoundedCorners(
-    #corner_radius=180,
-    #border_width=6,
     colour=colors["primary"]
 )
 
 # 2. Decoración para las ventanas inactivas
 borde_normal = RoundedCorners(
-    #corner_radius=180,
-    #border_width=6,
     colour=colors["surface_highest"] # Mismo radio y grosor, cambia el color
 )
 
 layout_theme = {
      "border_width":6,
      "margin":16,
-     #"border_focus":colors["primary"], #['orange'],
      "border_focus": borde_enfocado,
      "border_normal": borde_normal,  
-     #"border_normal":colors["surface_highest"], #['dark'],
      "single_border_width": 8,
  }
 
